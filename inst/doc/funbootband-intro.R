d51c34f8423b651072a347659e56c916dbf12f5c
Daniel Koska <dkoska@proton.me>
2026-09-18 14:30:08 +0000

    version 0.3.0

--- inst/doc/funbootband-intro.R
+++ inst/doc/funbootband-intro.R
@@ -7,8 +7,8 @@ knitr::opts_chunk$set(
 
 # Keep the vignette fast on CRAN by using tiny B
 is_cran <- !identical(tolower(Sys.getenv("NOT_CRAN")), "true")
-B_demo  <- if (is_cran) 25L else 1000L # bootstrap reps
-k.coef_demo <- 50L
+B_demo  <- if (is_cran) 25L else 500L # bootstrap reps
+k.coef_demo <- 4L
 
 set.seed(1)
 
@@ -16,99 +16,123 @@ set.seed(1)
 library(funbootband)
 
 set.seed(1)
-T <- 200
-n <- 10
+T <- 101L
+n <- 30L
 x <- seq(0, 1, length.out = T)
+mu_true <- 0.7 * sin(2 * pi * x) - 0.2 * cos(4 * pi * x)
 
-# Simulate smooth Gaussian-process-like curves of equal length
-mu  <- 10 * sin(2 * pi * x)
-ell <- 0.12; sig <- 3
-Kmat <- outer(x, x, function(s, t) sig^2 * exp(-(s - t)^2 / (2 * ell^2)))
-ev <- eigen(Kmat + 1e-8 * diag(T), symmetric = TRUE)
-Z  <- matrix(rnorm(T * n), T, n)
-Y  <- mu + ev$vectors %*% (sqrt(pmax(ev$values, 0)) * Z)
-Y  <- Y + matrix(rnorm(T * n, sd = 0.2), T, n) # observation noise
+generate_iid_curve <- function() {
+  mu_true +
+    rnorm(1, sd = 0.35) +
+    rnorm(1, sd = 0.30) * sin(2 * pi * x) +
+    rnorm(1, sd = 0.20) * cos(2 * pi * x) +
+    rnorm(1, sd = 0.15) * sin(4 * pi * x)
+}
+
+Y <- replicate(n, generate_iid_curve())
 
 ## ----iid-sim-plot-------------------------------------------------------------
 # Fit prediction and confidence bands
-fit_pred <- band(Y, type = "prediction", alpha = 0.11, iid = TRUE, B = B_demo, k.coef = k.coef_demo)
-fit_conf <- band(Y, type = "confidence", alpha = 0.11, iid = TRUE, B = B_demo, k.coef = k.coef_demo)
+fit_pred <- band(Y, type = "prediction", alpha = 0.10,
+                 iid = TRUE, B = B_demo, k.coef = k.coef_demo)
+fit_conf <- band(Y, type = "confidence", alpha = 0.10,
+                 iid = TRUE, B = B_demo, k.coef = k.coef_demo)
 
 ## ----iid-plot, fig.cap="Calculated prediction (blue) and confidence (gray) bands."----
-x_idx <- seq_len(fit_pred$meta$T)
 ylim  <- range(c(Y, fit_pred$lower, fit_pred$upper), finite = TRUE)
 
-plot(x_idx, fit_pred$mean, type = "n", ylim = ylim,
-     xlab = "Index (Time)", ylab = "Amplitude",
+plot(x, fit_pred$mean, type = "n", ylim = ylim,
+     xlab = "Normalized time", ylab = "Value",
      main = "Simultaneous bands (i.i.d.)")
 
-matlines(x_idx, Y, col = "gray70", lty = 1, lwd = 1)
-polygon(c(x_idx, rev(x_idx)), c(fit_pred$lower, rev(fit_pred$upper)),
+matlines(x, Y, col = grDevices::adjustcolor("gray40", 0.25), lty = 1)
+polygon(c(x, rev(x)), c(fit_pred$lower, rev(fit_pred$upper)),
         col = grDevices::adjustcolor("steelblue", alpha.f = 0.25), border = NA)
-polygon(c(x_idx, rev(x_idx)), c(fit_conf$lower, rev(fit_conf$upper)),
-        col = grDevices::adjustcolor("gray40", alpha.f = 0.3), border = NA)
-lines(x_idx, fit_pred$mean, col = "black", lwd = 1)
+polygon(c(x, rev(x)), c(fit_conf$lower, rev(fit_conf$upper)),
+        col = grDevices::adjustcolor("darkorange", alpha.f = 0.30), border = NA)
+lines(x, fit_pred$mean, lwd = 2)
+lines(x, mu_true, col = "red", lwd = 2, lty = 2)
 
 ## ----clustered, eval=TRUE-----------------------------------------------------
 library(funbootband)
 
 set.seed(2)
-T <- 200
-m <- c(5, 5)
-x <- seq(0, 1, length.out = T)
+K_subject <- 12L
+m <- rep(c(2L, 3L, 4L), length.out = K_subject)
+id <- rep(seq_len(K_subject), m)
 
-# Cluster-specific means
-mu <- list(
-  function(z) 8 * sin(2 * pi * z),
-  function(z) 8 * cos(2 * pi * z)
-)
+subject_effect <- sapply(seq_len(K_subject), function(i) {
+  rnorm(1, sd = 0.35) +
+    rnorm(1, sd = 0.30) * sin(2 * pi * x) +
+    rnorm(1, sd = 0.20) * cos(2 * pi * x)
+})
 
-# Generate curves with smooth within-cluster variation
-Bm <- cbind(sin(2 * pi * x), cos(2 * pi * x))
-gen_curve <- function(k) {
-  sc <- rnorm(ncol(Bm), sd = c(2.0, 1.5))
-  mu[[k]](x) + as.vector(Bm %*% sc)
+within_subject_effect <- function() {
+  rnorm(1, sd = 0.18) * sin(4 * pi * x) +
+    rnorm(1, sd = 0.12) * cos(4 * pi * x)
 }
 
-Ylist <- lapply(seq_along(m), function(k) {
-  sapply(seq_len(m[k]), function(i) gen_curve(k) + rnorm(T, sd = 0.6))
+Y <- sapply(seq_along(id), function(j) {
+  mu_true + subject_effect[, id[j]] + within_subject_effect()
 })
-Y <- do.call(cbind, Ylist)
-colnames(Y) <- unlist(mapply(
-  function(k, mk) paste0("C", k, "_", seq_len(mk)),
-  seq_along(m), m
-))
+
+trial <- ave(id, id, FUN = seq_along)
+colnames(Y) <- paste0("subject", id, "_trial", trial)
 
 
 # Fit prediction and confidence bands
-fit_pred <- band(Y, type = "prediction", alpha = 0.11, iid = FALSE, B = B_demo, k.coef = k.coef_demo)
-fit_conf <- band(Y, type = "confidence", alpha = 0.11, iid = FALSE, B = B_demo, k.coef = k.coef_demo)
+fit_pred <- band(Y, type = "prediction", alpha = 0.10, iid = FALSE,
+                 id = id, B = B_demo, k.coef = k.coef_demo)
+fit_conf <- band(Y, type = "confidence", alpha = 0.10, iid = FALSE,
+                 id = id, B = B_demo, k.coef = k.coef_demo)
+
+## ----clustered-meta-----------------------------------------------------------
+fit_pred$meta[c("target", "weighting", "bootstrap_unit", "n_clusters")]
 
 ## ----clustered-plot-----------------------------------------------------------
-x_idx <- seq_len(fit_pred$meta$T)
 ylim   <- range(c(Y, fit_pred$lower, fit_pred$upper), finite = TRUE)
 
-plot(x_idx, fit_pred$mean, type = "n", ylim = ylim,
-     xlab = "Index (Time)", ylab = "Amplitude",
+plot(x, fit_pred$mean, type = "n", ylim = ylim,
+     xlab = "Normalized time", ylab = "Value",
      main = "Simultaneous bands (clustered)")
 
-matlines(x_idx, Y, col = "gray70", lty = 1, lwd = 1)
-polygon(c(x_idx, rev(x_idx)), c(fit_pred$lower, rev(fit_pred$upper)),
+matlines(x, Y, col = grDevices::adjustcolor("gray40", 0.20), lty = 1)
+polygon(c(x, rev(x)), c(fit_pred$lower, rev(fit_pred$upper)),
         col = grDevices::adjustcolor("steelblue", alpha.f = 0.25), border = NA)
-polygon(c(x_idx, rev(x_idx)), c(fit_conf$lower, rev(fit_conf$upper)),
-        col = grDevices::adjustcolor("gray40", alpha.f = 0.3), border = NA)
-lines(x_idx, fit_pred$mean, col = "black", lwd = 1)
+polygon(c(x, rev(x)), c(fit_conf$lower, rev(fit_conf$upper)),
+        col = grDevices::adjustcolor("darkorange", alpha.f = 0.30), border = NA)
+lines(x, fit_pred$mean, lwd = 2)
+lines(x, mu_true, col = "red", lwd = 2, lty = 2)
+
+## ----clustered-future-coverage, eval = !is_cran-------------------------------
+# generate_new_subject_curve <- function() {
+#   new_subject_effect <-
+#     rnorm(1, sd = 0.35) +
+#     rnorm(1, sd = 0.30) * sin(2 * pi * x) +
+#     rnorm(1, sd = 0.20) * cos(2 * pi * x)
+#   new_curve_effect <-
+#     rnorm(1, sd = 0.18) * sin(4 * pi * x) +
+#     rnorm(1, sd = 0.12) * cos(4 * pi * x)
+#   mu_true + new_subject_effect + new_curve_effect
+# }
+# 
+# future_curves <- replicate(500L, generate_new_subject_curve())
+# covered <- apply(future_curves, 2L, function(curve) {
+#   all(curve >= fit_pred$lower & curve <= fit_pred$upper)
+# })
+# mean(covered)
 
 ## ----kcoef-mse-insample, eval = !is_cran--------------------------------------
 # # MSE vs k.coef for the i.i.d. example (uses Y from above)
 # 
 # fourier_basis <- function(T, K) {
-#   t <- seq_len(T)
+#   t <- 0:(T - 1L)
+#   denom <- T - 1L
 #   if (K == 0L) return(cbind(1))
 #   cbind(
 #     1,
-#     sapply(1:K, function(k) cos(2*pi*k*t/T)),
-#     sapply(1:K, function(k) sin(2*pi*k*t/T))
+#     sapply(1:K, function(k) cos(2*pi*k*t/denom)),
+#     sapply(1:K, function(k) sin(2*pi*k*t/denom))
 #   )
 # }
 # 
